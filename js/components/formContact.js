67b201db6b509bf7449e3f4f4330fa29de1636a2
Ariel Ricardo Suarez Baez <suarezariel86@gmail.com>
2022-04-05 00:48:56 -0300

    Te deje unos comentarios juli, vas muy bien!

--- js/components/formContact.js
+++ js/components/formContact.js
@@ -1,13 +1,17 @@
-
 // ************ Revisar porfa!! *************
-
 
 export const form = document.createElement("div");
 const contactForm = document.createElement("form");
-const formTitle = document.createElement("p");
+const formTitle =
+  document.createElement(
+    "p"
+  ); /*Esta etiqueta podria ser un <h3> o <legend> por ej,
+pero no <p> porque te estas refiriendo a un titulo y tiene su debida jerarquia sobre todo para el SEO*/
 formTitle.textContent = "Contactate con nosotros";
-formTitle.classList.add("form__title")
-
+formTitle.classList.add(
+  "form__title"
+); /* Esta muy bien la implementacion de la clase!, ahora te falta crearla.
+Acordate que las clases del componente van en scss/components y las del layout general de la pagina que estes haciendo en scss/pages */
 
 form.appendChild(contactForm);
 
--- js/pages/contact.js
+++ js/pages/contact.js
@@ -1,7 +1,9 @@
 //************************CONTACTO***********************************/
 import { navBarAux } from "../components/navBarAux.js";
-import { form } from "../components/formContact";
-
+import { form } from "../components/formContact.js"; /* aca te agregue la extension .js. Recorda que este tipo de import
+requieren que le especifiques la extension del archivo para que no se te rompa el codigo. A medida que hagas cambios chequea
+con el inspector del navegador que mensajes te tira, asi te ayuda a entender que podes estar haciendo mal y de paso
+practicas la lectura de errores que lamentablemente vas a vivir teniendolos (te lo digo de primera mano jajajaj) */
 
 const bodyContact = document.getElementById("body-contact");
 const headerContact = document.getElementById("header-contact");
@@ -9,4 +11,13 @@ const mainContact = document.getElementById("main-contact");
 headerContact.appendChild(navBarAux);
 bodyContact.appendChild(headerContact);
 bodyContact.appendChild(mainContact);
+
+/*mainContact.appendChild(form);*/
+/* como primera accion te recomiendo que siempre que crees un componente lo exportes desde su archivo,
+posteriormente lo importes en su respectivo archivo modelo y le hagas el appendChild como te hice aca a donde lo necesites tener asi ya despues trabajas
+tranquilo solamente sobre el componente y vas viendo los cambios que haces */
 //*******************************************************************/
+
+/* Te deje comentarios en los archivos que creastes para que los revises tranqui*/
+
+/********* VAS MUY BIEN JULIAN! A SEGUIRLE METIENDO! ***********/
