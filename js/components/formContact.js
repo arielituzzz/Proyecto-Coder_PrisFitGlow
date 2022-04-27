53396e92f056a7ce3ce652ffd14e25021bc4b0c7
Ariel Ricardo Suarez Baez <suarezariel86@gmail.com>
2022-04-27 08:20:58 -0300

    solucionado problema con evento

--- js/components/formContact.js
+++ js/components/formContact.js
@@ -3,15 +3,13 @@ const contactForm = document.createElement("form");
 contactForm.setAttribute("id", "form");
 form.appendChild(contactForm);
 
-
 const formLeft = document.createElement("div");
 contactForm.appendChild(formLeft);
-formLeft.classList.add("form__left")
-
+formLeft.classList.add("form__left");
 
 const formRight = document.createElement("div");
 contactForm.appendChild(formRight);
-formRight.classList.add("form__right")
+formRight.classList.add("form__right");
 
 const formTitle = document.createElement("h3");
 formTitle.textContent = "Contactate con nosotros";
@@ -49,28 +47,28 @@ submit.classList.add("form__left__submit");
 submit.setAttribute("id", "button");
 formLeft.appendChild(submit);
 
-
 //Api
 
-// const btn = document.getElementById('button');
-// const emailJS = document.getElementById('form');
-// emailJS.addEventListener('submit', function (event) {
-//     event.preventDefault();
+const btn = document.getElementById("button");
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-//     btn.value = 'Sending...';
+  btn.value = "Sending...";
 
-//     const serviceID = 'default_service';
-//     const templateID = 'template_2dr450n';
+  const serviceID = "default_service";
+  const templateID = "template_2dr450n";
 
-//     emailjs.sendForm(serviceID, templateID, this)
-//         .then(() => {
-//             btn.value = 'Send Email';
-//             alert('Sent!');
-//         }, (err) => {
-//             btn.value = 'Send Email';
-//             alert(JSON.stringify(err));
-//         });
-// });
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
 
 const formImg = document.createElement("img");
 formImg.src = "/images/paul-hanaoka-HbyYFFokvm0-unsplash.png";
@@ -79,8 +77,6 @@ formRight.appendChild(formImg);
 const logos = document.createElement("div");
 formRight.appendChild(logos);
 logos.classList.add("form__right__logos");
-
-
 
 const facebook = document.createElement("div");
 logos.appendChild(facebook);
@@ -95,9 +91,6 @@ textoFacebook.textContent = "Facebook";
 facebook.appendChild(textoFacebook);
 textoFacebook.classList.add("form__right__logos__texto");
 
-
-
-
 const instagram = document.createElement("div");
 logos.appendChild(instagram);
 instagram.classList.add("form__right__logos__instagram");
@@ -110,7 +103,6 @@ const textoInstagram = document.createElement("h4");
 textoInstagram.textContent = "Instagram";
 instagram.appendChild(textoInstagram);
 textoInstagram.classList.add("form__right__logos__texto");
-
 
 const mail = document.createElement("div");
 logos.appendChild(mail);
